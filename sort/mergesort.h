726fb19131782af43b59840816a152ea046f521a
Michael Wong <34909192+mwck46@users.noreply.github.com>
2022-06-25 00:06:43 +0800

    remove useless dependency

--- sort/mergesort.h
+++ sort/mergesort.h
@@ -3,14 +3,12 @@
 
 #include <stdlib.h>
 #include <string.h> 
-#include "ArrayEx.h"
 
-using namespace util;
-
+// Not inline implementation of mergesort, not memory efficient
 class mergesort
 {
 public:
-  void sort(int* arr, int arrSize)
+  static void sort(int* arr, int arrSize)
   {
     if (arrSize == 1)
     {
@@ -20,21 +18,18 @@ public:
     int leftArrSize = arrSize / 2;
     int* leftArr = (int*)malloc(leftArrSize * sizeof(int));
     memcpy(leftArr, arr, leftArrSize * sizeof(int));
-    //_printArray(leftArr, leftArrSize);
     sort(leftArr, leftArrSize);
 
     int rightArrSize = arrSize - leftArrSize;
     int* rightArr = (int*)malloc(rightArrSize * sizeof(int));
     memcpy(rightArr, arr + leftArrSize, rightArrSize * sizeof(int));
-    //_printArray(rightArr, rightArrSize);
     sort(rightArr, rightArrSize);
 
-    merge(arr, leftArr, leftArrSize, rightArr, rightArrSize);
-    ArrayEx::cout(arr, arrSize);
+    mergesort::merge(arr, leftArr, leftArrSize, rightArr, rightArrSize);
   }
 
 private:
-  void merge(int* dest, int* arr1, int arr1Size, int* arr2, int arr2Size)
+  static void merge(int* dest, int* arr1, int arr1Size, int* arr2, int arr2Size)
   {
     int i = 0, j = 0, k = 0;
     while (i < arr1Size && j < arr2Size)
@@ -57,8 +52,8 @@ private:
       dest[k++] = arr2[j++];
     }
   }
-
-
 };
 
 #endif
+
+
